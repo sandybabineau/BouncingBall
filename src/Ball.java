0a1cffffbe493024c8348a47ab4ece85b9b55512
sandy.babineau24 <sandybabineau64@gmail.com>
2024-02-02 08:09:47 -0600

    I made it worse

--- src/Ball.java
+++ src/Ball.java
@@ -4,52 +4,73 @@ import java.awt.*;
 public class Ball {
     private int size;
     private Color color;
-    private int xspeed,yspeed;
-    private int x,y;
+    private int xspeed, yspeed;
+    private int x, y;
 
-    public Ball(int x, int y){
+    public Ball(int x, int y) {
         this.x = x;
         this.y = y;
-        size = (int)((Math.random()*10)+10);
-        color = new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
-        xspeed = (int)((Math.random()*6)+1);
-        yspeed = (int)((Math.random()*6)+1);
+        size = (int) ((Math.random() * 20) + 30);
+        color = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
+        xspeed = (int) ((Math.random() * 6) + 1);
+        yspeed = (int) ((Math.random() * 6) + 1);
 //        xspeed = 1;
 //        yspeed = 1;
     }
-    public int getX(){
+
+    public int getX() {
         return x;
     }
-    public int getY(){
+
+    public int getY() {
         return y;
     }
-    public int getSize(){
+
+    public int getSize() {
         return size;
     }
-    public void reverse(){
+
+    public void reverse() {
         xspeed *= -1;
         yspeed *= -1;
     }
-    public void move(){
-        x+=xspeed;
-        y+=yspeed;
+
+    public void ranSpeed() {
+        xspeed = (int) (Math.random() * 3) + 1;
+        yspeed = (int) (Math.random() * 3) + 1;
     }
-    public void draw(Graphics g2){
+
+    public void move() {
+        x += xspeed;
+        y += yspeed;
+    }
+
+    public void draw(Graphics g2) {
         g2.setColor(color);
-        g2.fillOval(x,y,size,size);
+        g2.fillOval(x, y, size, size);
     }
 
-    public void bounce(MyPanel panel){
-        if(x>(panel.getWidth()-size)||x<0){
+    public void bounce(MyPanel panel) {
+        if (x > (panel.getWidth() - size) || x < size) {
+            ranSpeed();
             xspeed *= -1;
-            color = new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+            if (x > panel.getWidth()) {
+                x = panel.getWidth();
+            } else if (x < size) {
+                x = size;
+            }
+            color = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
         }
-        if(y>(panel.getWidth()-size)||y<0){
+        if (y > (panel.getWidth() - size) || y < size) {
+            ranSpeed();
             yspeed *= -1;
-            color = new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+            if (y > panel.getHeight() - size) {
+                y = panel.getHeight();
+            } else if (y < size) {
+                y = size;
+            }
+            color = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
         }
 
     }
-
-
 }
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -12,7 +12,7 @@ Ball[] wow;
     public MyPanel(){
         setBackground(Color.PINK);
         myBall = new Ball(200,200);
-        wow = new Ball[1000];
+        wow = new Ball[20];
         for (int i = 0; i<wow.length;i++){
             wow[i]=new Ball((int)(Math.random()*500),(int)(Math.random()*500));
         }
